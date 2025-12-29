8f676e08d5efca7af052032e43b48452da536838
yaduit <yk226411@gmail.com>
2025-12-29 11:36:26 +0530

    implemeted register user

---
+++ api/src/modules/auth/auth.controller.js
@@ -0,0 +1,35 @@
+import userModel from '../users/user.model.js'
+import bcrypt from 'bcrypt'
+
+export const registerUser = async(req, res)=>{
+    try{
+        let{name,email,password} = req.body;
+        if(!name||!email||!password){
+           return res.status(400).json({message: 'All fields are required'})
+        }
+        name = name.trim();
+        email = email.trim().toLowerCase();
+        
+        const userExist = await userModel.findOne({email})
+        if(userExist){
+             return res.status(409).json({message: 'user already exist'});
+        }
+
+        const passwordHash = await bcrypt.hash(password,12);
+        const user = await userModel.create({
+            name,
+            email,
+            passwordHash
+        });
+        return res.status(201).json({
+            message: 'user registered sucessfully', user:{
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role
+            }})
+    }catch(err){
+        console.log(err.message)
+        return res.status(500).json({message: 'internal server error'})
+    }
+}
