3fbb843639c238a0c663a78d22c335d3174efa33
yaduit <yk226411@gmail.com>
2025-12-30 13:04:12 +0530

    cookieparser added

--- api/src/app.js
+++ api/src/app.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import cors from 'cors'
 import authRoutes from './modules/auth/auth.routes.js'
-
+import cookieParser from 'cookie-parser'
 const app = express();
 
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
+app.use(cookieParser());
 
 app.get("/", function(req,res){
     res.send("Hello from Express server!");
