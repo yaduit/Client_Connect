24344f50725ea0c66b0fbf1dcd2dfa13a73efef1
yaduit <yk226411@gmail.com>
2026-01-01 13:03:00 +0530

    feat: added protected route logic authMiddleware

---
+++ api/src/middlewares/auth.middleware.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken';
+
+const authMiddleware = async function (req,res,next){
+    try{
+        const token = req.cookies?.token;
+        if(!token){
+            return req.status(401).json({message: 'Not Authenticated'})
+        }
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        req.user={
+            id: decoded.id,
+            role: decoded.role
+        }
+
+        next();
+    }catch(error){
+        return res.status(401).json({message: "invalid or expired token"})
+    }
+}
+
+export default authMiddleware
