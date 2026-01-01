a2bc0970fc6017655ae90e1e21dce1c243e79098
yaduit <yk226411@gmail.com>
2026-01-01 13:03:55 +0530

    feat: added protected test route

--- api/src/modules/auth/auth.routes.js
+++ api/src/modules/auth/auth.routes.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import { registerUser, loginUser } from './auth.controller.js';
+import authMiddleware from '../../middlewares/auth.middleware.js';
 const router = express.Router();
 
 router.post('/register', registerUser );
 router.post('/login', loginUser);
-
+router.get('/me',authMiddleware,function(req,res){
+    res.status(201).json({message: 'Authenticated user', user: req.user})
+   })
 export default router
