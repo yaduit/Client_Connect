9a2ca77cdf1d528bcc5916dac543b1690df2916a
yaduit <yk226411@gmail.com>
2026-01-04 13:57:31 +0530

    chore : removed unused rolemiddleware

--- api/src/modules/auth/auth.routes.js
+++ api/src/modules/auth/auth.routes.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import { registerUser, loginUser,logoutUser } from './auth.controller.js';
 import authMiddleware from '../../middlewares/auth.middleware.js';
-import roleMiddleware from '../../middlewares/role.middleware.js';
 
 const router = express.Router();
 
