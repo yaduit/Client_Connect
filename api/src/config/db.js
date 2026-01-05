8b54cd03daf5dca800f109d09443e1bf538b5af7
yaduit <yk226411@gmail.com>
2026-01-05 12:18:01 +0530

    feat: added server selection timeout

--- api/src/config/db.js
+++ api/src/config/db.js
@@ -2,7 +2,9 @@ import mongoose from 'mongoose'
 
 const connectDB = async function(){
     try{
-        await mongoose.connect(process.env.MONGO_URI)
+        await mongoose.connect(process.env.MONGO_URI,{
+             serverSelectionTimeoutMS: 5000,
+        })
         console.log("connected to db")
     }catch(error){
         console.log('db connection failed',error.message);
