779a39bdfdeb734f7808be40418bd3e5d20eb7a6
yaduit <yk226411@gmail.com>
2026-01-07 12:19:23 +0530

    chore: deleted unwanted text

--- api/src/app.js
+++ api/src/app.js
@@ -8,7 +8,7 @@ import cookieParser from 'cookie-parser'
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173', // your frontend
+  origin: 'http://localhost:5173',
   credentials: true
 }));
 
