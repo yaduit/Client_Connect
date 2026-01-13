88ab496b762e4d446f90a6c691d13f61f46f91ca
yaduit <yk226411@gmail.com>
2026-01-13 14:48:15 +0530

    feat: add initial homepage

---
+++ clientFe/src/pages/homePage.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/layout/navbar.jsx";
+
+const HomePage = () => {
+  return (
+    <>
+      <Navbar />
+      {/* other sections */}
+    </>
+  );
+};
+
+export default HomePage;
