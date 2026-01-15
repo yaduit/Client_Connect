b34dabc71bdec61c16567c81735dabbb28784177
yaduit <yk226411@gmail.com>
2026-01-15 18:30:15 +0530

    feat: add topservicepage

--- clientFe/src/pages/homePage.jsx
+++ clientFe/src/pages/homePage.jsx
@@ -1,7 +1,7 @@
 import Navbar from "../components/layout/navbar.jsx";
 import HeroSection from "../components/layout/heroSection.jsx";
 import CategoryGrid from "../components/layout/categoryGrid.jsx";
-import TopServices from "../components/layout/topServices.jsx";
+import TopServicesNearYou from "../components/layout/topServices.jsx";
 import HowItWorks from "../components/layout/howitWorks.jsx";
 import Footer from "../components/layout/footer.jsx";
 const HomePage = () => {
@@ -10,7 +10,7 @@ const HomePage = () => {
       <Navbar />
       <HeroSection/>
       <CategoryGrid/>
-      <TopServices/>
+      <TopServicesNearYou/>
       <HowItWorks/>
       <Footer/>
       {/* other sections */}
