4734ce4131f1f842d73b2c4004a29f26f0ff7a35
yaduit <yk226411@gmail.com>
2026-01-23 12:33:30 +0530

    refactor: provider APIs and add provider dashboard

--- clientFe/src/api/providers.api.js
+++
@@ -1,6 +0,0 @@
-import axios from './axios.js'
-
- export const getProviderByIdApi = async (id) =>{
-    const res = await axios.get(`/providers/${id}`);
-    return res.data;
- };
---
+++ clientFe/src/pages/provider/providerDashBoard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ProviderDashBoard() {
+  return (
+    <div>providerDashBoard</div>
+  )
+}
+
+export default ProviderDashBoard;
