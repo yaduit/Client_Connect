7c733b09f586f4fa9999a607df94f9e8d68c5a6c
yaduit <yk226411@gmail.com>
2026-01-25 17:45:34 +0530

    fix: main orderfix

--- clientFe/src/main.jsx
+++ clientFe/src/main.jsx
@@ -1,18 +1,16 @@
-
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import { LocationProvider } from './context/location/locationProvider.jsx'
-import AuthProvider from './context/auth/authProvider.jsx';
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { LocationProvider } from "./context/location/locationProvider.jsx";
+import AuthProvider from "./context/auth/authProvider.jsx";
 //Temporary strictmode removal//
-createRoot(document.getElementById('root')).render(
- 
-    <BrowserRouter>
-    <AuthProvider> 
-    <LocationProvider>
-      <App />
-    </LocationProvider>
-     </AuthProvider>
-    </BrowserRouter>
-)
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <AuthProvider>
+      <LocationProvider>
+        <App />
+      </LocationProvider>
+    </AuthProvider>
+  </BrowserRouter>,
+);
